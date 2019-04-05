65e31585cd2b43981cc2b38cff521b4e43fa4d93
Fernando Zabin <fernando.zabin@gmail.com>
2019-04-05 09:15:49 -0300

    Add routes and update app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+import routes from './routes'
 
 class App {
   public express: express.Application
@@ -25,9 +27,7 @@ class App {
   }
 
   private routes (): void {
-    this.express.get('/', (req, res): Response => {
-      return res.send('Hello, world!')
-    })
+    this.express.use(routes)
   }
 }
 
